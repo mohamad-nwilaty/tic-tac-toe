740b40620a3938daf72a80274c4aeded9f14de5d
Mohamad nwilaty <mhmadnwilaty@gmail.com>
2024-08-28 02:39:45 +0300

    finished the game

--- src/Game.java
+++ src/Game.java
@@ -1,29 +1,33 @@
 import java.util.Scanner;;
 public class Game {
 
-    static int[] board = new int[9];
+    static char[] board = new char[9];
     static char next = 'O' ;
     static boolean status = true ;
     public static void main(String[] args) throws Exception {
         
 
-        int initial = 1 ;
+        
         for(int i=0 ; i<9 ; i++){
-            board[i] = initial ;
-            initial++ ;
+            board[i] = (char) (i + '1') ; // here it convets from ascii thats why we add '1' 
         }
+
         System.out.println("The first move is O :");
         while (status) {
-            printBoard();
+            if(!checkWining())
+            {
+                System.out.println("Next to play: " + next );
+                printBoard();
+                int move = input();
+                if(validMove(move) ){
+                    board[move -1] = next ;
+                    changPlay(next);
+                }
+            }
 
-            if(validMove(input())){
-                System.out.println("yes");
-            }
-            else{
-                System.out.println("No");
-            }
         }
-        
+        changPlay(next);
+        System.out.println(next + " has won");
         
        
     }
@@ -57,24 +61,63 @@ public class Game {
             return false ;
         }
         for(int i=0 ; i<9 ; i++){
-            if(input == board[i]) return true;
-        }
+            if(input == (int) (board[i] - '0')) return true; // here we subtracy '0' because when we type cast a char 
+        }                                                    // java converts it to ascii                
         System.out.println("this move is already taken! ");
         return false;
     }
 
     public static int input(){ // this function takes input and insures that it is an number else itreturns -1 which will 
-                                // return false in the validMove methode .
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        String line = scan.next();
+    // return false in the validMove methode .
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Enter a number: ");
+    String line = scan.next();
         try{
             int n = Integer.valueOf(line);
             return n;
         }
         catch(Exception e){
-            System.out.println("You didnt enter a Number dummy !");
+        System.out.println("You didnt enter a Number dummy !");
         }
         return -1 ;
     }
+
+    public static void changPlay(char Next){
+        if(Next == 'O') next = 'X';
+        else next = 'O';
+    }
+
+
+    public static boolean checkHorizontal(){
+        for(int i=0 ; i <=6 ; i+= 3){
+            if(board[i] == board[i+1] && board[i] == board[i+2]) return true ;
+            
+        }
+        return false ; 
+    }
+
+
+    public static boolean checkVertical(){
+        for(int i=0 ; i <3 ; i++){
+            if(board[i] == board[i+3] && board[i] == board[i+6]) return true ;
+            
+        }
+        return false ; 
+    }
+
+    public static boolean checkDiagonal(){
+        if(board[0] == board[4] && board[0] == board[8]) return true ;
+        if(board[2] == board[4] && board[2] == board[6]) return true ;
+        return false ; 
+    }
+
+    public static boolean checkWining(){
+        if(checkDiagonal() || checkHorizontal() || checkVertical() ){
+            System.out.println("Game over !");
+            status = false ;
+            return true ;
+        } 
+        return false ;
+    }
+
 }
